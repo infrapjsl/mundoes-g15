40feb53e9e3e376450345db30ba8746af5846a3c
IleanaRoche <ileale.roche@gmail.com>
2024-11-26 10:19:05 -0300

    Update ec2_user_data.sh

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 echo "Installing AWS CLI"
+# Update package manager sources
+sudo apt-get update
+#Install unzip
+sudo apt install unzip
+# Download AWS CLI https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+cd aws
+./install
+aws --version
 
 echo "Installing kubectl"
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
@@ -29,6 +39,11 @@ sudo systemctl start docker.service
 
 
 echo "Installing Helm"
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install Helm
 
 echo "Installing terraform "
 sudo yum install -y yum-utils
